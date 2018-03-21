3818e6343ad400acb3b29782492bfb0c4d969880
Varghese Thomas <varghesethomase@gmail.com>
2018-03-21 21:33:20 +0530

    Basic header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = props => (
+  <nav className="navabar">
+    <header className="header" />
+  </nav>
+);
+
+export default Header;
