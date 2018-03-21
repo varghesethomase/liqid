9df5b548b2373d6bfda320c447c27f0f688c84bf
Varghese Thomas <varghesethomase@gmail.com>
2018-03-21 22:23:06 +0530

    Landing component

---
+++ src/containers/Landing/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import './Landing.css';
+
+class LandingPage extends Component {
+  render() {
+    return <div className="landing-screen" />;
+  }
+}
+
+export default LandingPage;
