a64defaa758ba815a00432dd9e03937bfe2bbd48
Varghese Thomas <varghesethomase@gmail.com>
2018-03-21 23:27:41 +0530

    Adding a header and start button to landing

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import Button from '../../components/Button';
 import './Landing.css';
 
 class LandingPage extends Component {
   render() {
-    return <div className="landing-screen" />;
+    return (
+      <div className="landing-screen">
+        <div className="content">
+          <h1>Simple Survey</h1>
+          <div>
+            <Button to="/">START SURVEY</Button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
