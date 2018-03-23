701aa3d36ed1888b73a23962f0035939659af25d
Varghese Thomas <varghesethomase@gmail.com>
2018-03-23 21:39:26 +0530

    passing questions via prop

--- src/components/QuestionTypography/index.js
+++ src/components/QuestionTypography/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import './QuestionTypography.css';
 
-const QuestionTypography = props => (
-  <h1 className="question">Sample question looks like this!</h1>
-);
+const QuestionTypography = props => <h1 className="question">{props.text}</h1>;
 
 export default QuestionTypography;
