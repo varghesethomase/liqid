7d71b72ddabc22d46b419205c3d2afdaec4f50eb
Varghese Thomas <varghesethomase@gmail.com>
2018-03-23 07:15:26 +0530

    Question display component

---
+++ src/components/QuestionTypography/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './QuestionTypography.css';
+
+const QuestionTypography = props => (
+  <h1 className="question">Sample question looks like this!</h1>
+);
+
+export default QuestionTypography;
