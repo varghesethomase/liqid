2315b477462ce1e48a5eee304ed85f8f2e75782a
Varghese Thomas <varghesethomase@gmail.com>
2018-03-24 15:33:38 +0530

    moved from function component to class component as props were required throughout

--- src/components/AnswerField/index.js
+++ src/components/AnswerField/index.js
@@ -1,45 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import './AnswerField.css';
 
-function renderField(type, options) {
-  if (type === 'text') {
-    return <textarea className="answer-text-field" />;
-  } else if (type === 'dropdown') {
-    return (
-      <select>
-        {options.map(option => (
-          <option value={option.value} key={option.value}>
-            {option.name}
-          </option>
-        ))}
-      </select>
-    );
-  } else if (type === 'radiobutton') {
-    return (
-      <div>
-        {options.map(option => (
-          <div key={option.value}>
-            <input
-              type="radio"
-              name={option.name}
-              value={option.value}
-              id={option.value}
-            />
-            <label htmlFor={option.value}>{option.name}</label>
-          </div>
-        ))}
-      </div>
-    );
+class AnswerField extends Component {
+  renderField = () => {
+    const { field: { type, options }, updateAnswer } = this.props;
+    if (type === 'text') {
+      return <textarea className="answer-text-field" onChange={updateAnswer} />;
+    } else if (type === 'dropdown') {
+      return (
+        <select onChange={updateAnswer}>
+          {options.map(option => (
+            <option value={option.value} key={option.value}>
+              {option.name}
+            </option>
+          ))}
+        </select>
+      );
+    } else if (type === 'radiobutton') {
+      return (
+        <div>
+          {options.map(option => (
+            <div key={option.value}>
+              <input
+                type="radio"
+                name={option.name}
+                value={option.value}
+                id={option.value}
+                onClick={updateAnswer}
+              />
+              <label htmlFor={option.value}>{option.name}</label>
+            </div>
+          ))}
+        </div>
+      );
+    }
+  };
+
+  render() {
+    return <div className="answer-field-wrapper">{this.renderField()}</div>;
   }
 }
 
-const AnswerField = props => {
-  return (
-    <div className="answer-field-wrapper">
-      {renderField(props.field.type, props.field.value)}
-    </div>
-  );
+AnswerField.propTypes = {
+  field: PropTypes.object.isRequired,
+  updateAnswer: PropTypes.func.isRequired
 };
 
 export default AnswerField;
