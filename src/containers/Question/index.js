b21ec815b81a1240e901ba567bbaf03cc826d393
Varghese Thomas <varghesethomase@gmail.com>
2018-03-24 15:36:10 +0530

    updating for card footer handling button click adding with router, adding state for saving answer

--- src/containers/Question/index.js
+++ src/containers/Question/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import Header from '../../components/Header';
 import QuestionCard from '../../components/QuestionCard';
@@ -7,15 +8,36 @@ import ProgressIndicator from '../../components/ProgressIndicator';
 import QuestionTypography from '../../components/QuestionTypography';
 import AnswerField from '../../components/AnswerField';
 import CardFooter from '../../components/CardFooter';
+import { submitAnswer } from './actions';
 import './Question.css';
 
 class Question extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answer: ''
+    };
+  }
   goBack = () => {
     this.props.history.goBack();
   };
+
   completedPercent = () =>
     this.props.questionData.currentQuestion /
     this.props.questionData.questions.length;
+
+  updateAnswer = event => {
+    this.setState({ answer: event.target.value });
+  };
+
+  submitResponse = () => {
+    const currentQuestion = this.props.questionData.currentQuestion;
+    this.props.submitAnswer(currentQuestion, this.state.answer);
+    this.props.history.push(
+      `/question/${this.props.questionData.questions[currentQuestion + 1].id}`
+    );
+  };
+
   render() {
     const { questionData } = this.props;
     const currentQuestion =
@@ -27,8 +49,14 @@ class Question extends Component {
           <QuestionCard>
             <ProgressIndicator percent={this.completedPercent} />
             <QuestionTypography text={currentQuestion.question} />
-            <AnswerField field={currentQuestion.fieldProperties} />
-            <CardFooter next="/" back={this.goBack} />
+            <AnswerField
+              field={currentQuestion.fieldProperties}
+              updateAnswer={this.updateAnswer}
+            />
+            <CardFooter
+              clickSubmit={this.submitResponse}
+              clickBack={this.goBack}
+            />
           </QuestionCard>
         </div>
       </div>
@@ -41,7 +69,9 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  changeQuestion: value => dispatch()
+  submitAnswer: (id, answer) => dispatch(submitAnswer(id, answer))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Question);
+export default connect(mapStateToProps, mapDispatchToProps)(
+  withRouter(Question)
+);
