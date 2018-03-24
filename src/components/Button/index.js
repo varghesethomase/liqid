3ecc063034474f1ba4ed6a298ed0cb40ada097b0
Varghese Thomas <varghesethomase@gmail.com>
2018-03-24 21:14:10 +0530

    Disabling button based on value

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,14 +3,21 @@ import PropTypes from 'prop-types';
 
 import './Button.css';
 
-const Button = props => (
-  <a className="button" onClick={props.onClick}>
-    {props.children}
-  </a>
-);
+const Button = props => {
+  console.log(props);
+  return (
+    <button
+      className="button"
+      disabled={props.value === ''}
+      onClick={props.buttonClick}
+    >
+      {props.children}
+    </button>
+  );
+};
 
 Button.propTypes = {
-  onClick: PropTypes.func.isRequired,
+  buttonClick: PropTypes.func.isRequired,
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.element])
     .isRequired
 };
