53142d1da62110776d28be431e69b20588fc49b8
Varghese Thomas <varghesethomase@gmail.com>
2018-03-25 16:54:50 +0530

    prop validations and rewriting as a stateless component

--- src/components/ProgressIndicator/index.js
+++ src/components/ProgressIndicator/index.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import './ProgressIndicator.css';
 
-class ProgressIndicator extends Component {
-  render() {
-    return (
-      <div className="progress">
-        {this.props.currentQuestion} / {this.props.totalQuestions}
-        <svg viewBox="0 0 50 50" className="svg-box">
-          <circle
-            className="progress-circle"
-            cx="25"
-            cy="25"
-            r="22"
-            ref={element => (this.svgCircle = element)}
-            style={{
-              strokeDasharray: '138.2304',
-              strokeDashoffset: 138.2304 * (1 - this.props.progressPercentage)
-            }}
-          />
-        </svg>
-      </div>
-    );
-  }
-}
+const ProgressIndicator = props => (
+  <div className="progress">
+    {props.currentQuestion} / {props.totalQuestions}
+    <svg viewBox="0 0 50 50" className="svg-box">
+      <circle
+        className="progress-circle"
+        cx="25"
+        cy="25"
+        r="22"
+        style={{
+          strokeDasharray: '138.2304',
+          strokeDashoffset: 138.2304 * (1 - props.progressPercentage),
+        }}
+      />
+    </svg>
+  </div>
+);
+
+ProgressIndicator.propTypes = {
+  currentQuestion: PropTypes.number.isRequired,
+  totalQuestions: PropTypes.number.isRequired,
+  progressPercentage: PropTypes.number.isRequired,
+};
 
 export default ProgressIndicator;
