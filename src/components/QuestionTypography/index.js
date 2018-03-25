2a4bbf49506e5e459979698dc76f4b49eb4582ef
Varghese Thomas <varghesethomase@gmail.com>
2018-03-25 16:56:48 +0530

    prop validations

--- src/components/QuestionTypography/index.js
+++ src/components/QuestionTypography/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './QuestionTypography.css';
 
 const QuestionTypography = props => <h1 className="question">{props.text}</h1>;
+
+QuestionTypography.propTypes = {
+  text: PropTypes.string.isRequired,
+};
 
 export default QuestionTypography;
