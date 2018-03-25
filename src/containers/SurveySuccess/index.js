641b287f2aa6f7c79848ba9268e89c807924914e
Varghese Thomas <varghesethomase@gmail.com>
2018-03-25 15:37:31 +0530

    Adding navigate back and fixing the console errors and fixing designs

--- src/containers/SurveySuccess/index.js
+++ src/containers/SurveySuccess/index.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import Header from '../../components/Header';
 import Card from '../../components/Card';
 import QuestionTypography from '../../components/QuestionTypography';
 import './SurveySuccess.css';
 
 class SurveySuccess extends Component {
+  constructor(props) {
+    super(props);
+    this.navigateBack = this.navigateBack.bind(this);
+  }
+  navigateBack() {
+    this.props.history.goBack();
+  }
   render() {
     const { questions } = this.props;
     return (
-      <div className="survey-success">
-        {questions.map(question => (
-          <Card>
-            <QuestionTypography text={question.question} />
-            <div className="answer">{question.answer}</div>
-          </Card>
-        ))}
-      </div>
+      <React.Fragment>
+        <Header goBack={this.navigateBack} />
+        <div className="survey-success-wrapper">
+          {questions.map(question => (
+            <Card className="survey-success-card" key={question.id}>
+              <QuestionTypography text={question.question} />
+              <div className="answer">{question.answer}</div>
+            </Card>
+          ))}
+        </div>
+      </React.Fragment>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  questions: state.question.questionData.questions
+  questions: state.question.questionData.questions,
 });
 
-export default connect(mapStateToProps, null)(SurveySuccess);
+SurveySuccess.propTypes = {
+  questions: PropTypes.array.isRequired,
+  history: PropTypes.object.isRequired,
+};
+
+export default connect(mapStateToProps, null)(withRouter(SurveySuccess));
