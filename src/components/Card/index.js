e5be67319cc5b929ec9e23f659dcd70ac986d5ce
Varghese Thomas <varghesethomase@gmail.com>
2018-03-25 16:50:06 +0530

    Prop validations

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './Card.css';
 
 const Card = props => (
   <div className={`card ${props.className}`}>{props.children}</div>
 );
+
+Card.defaultProps = {
+  className: '',
+};
+
+Card.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.element,
+  ]).isRequired,
+};
 
 export default Card;
