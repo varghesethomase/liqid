fdde10c2d49dcdbfa96eaa0432626bca755a69e8
Varghese Thomas <varghesethomase@gmail.com>
2018-03-26 13:22:08 +0530

    updating tests for click event

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -7,3 +7,10 @@ it('renders the button', () => {
   const wrapper = shallow(<Button buttonClick={onButtonClick}>HELLO</Button>);
   expect(wrapper).toMatchSnapshot();
 });
+
+it('responds to click', () => {
+  const onButtonClick = jest.fn();
+  const wrapper = shallow((<Button buttonClick={onButtonClick}>TEXT</Button>));
+  wrapper.find('button').simulate('click');
+  expect(wrapper).toMatchSnapshot();
+});
