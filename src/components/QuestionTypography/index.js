531759423b91357b0f3f634810d775b31be229e9
Varghese Thomas <varghesethomase@gmail.com>
2018-03-26 16:13:37 +0530

    class name udpates

--- src/components/QuestionTypography/index.js
+++ src/components/QuestionTypography/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import './QuestionTypography.css';
 
-const QuestionTypography = props => <h1 className="question">{props.text}</h1>;
+const QuestionTypography = props => <h1 className="question-typography">{props.text}</h1>;
 
 QuestionTypography.propTypes = {
   text: PropTypes.string.isRequired,
