4cb8bb7dcda4212e19ca5b891a01c7a5a73162b3
Varghese Thomas <varghesethomase@gmail.com>
2018-03-26 07:04:47 +0530

    Converting to stateless component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
@@ -10,27 +10,18 @@ import './App.css';
 const AsyncLanding = asyncComponent(() => import('./containers/Landing'));
 const AsyncQuestion = asyncComponent(() => import('./containers/Question'));
 const AsyncSurveySuccess = asyncComponent(() =>
-  import('./containers/SurveySuccess')
-);
+import('./containers/SurveySuccess'));
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <div className="app-wrapper">
-            <Route exact path="/" component={AsyncLanding} />
-            <Route exact path="/question/:id" component={AsyncQuestion} />
-            <Route
-              exact
-              path="/survey-success"
-              component={AsyncSurveySuccess}
-            />
-          </div>
-        </ConnectedRouter>
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div className="app-wrapper">
+        <Route exact path="/" component={AsyncLanding} />
+        <Route exact path="/question/:id" component={AsyncQuestion} />
+        <Route exact path="/survey-success" component={AsyncSurveySuccess} />
+      </div>
+    </ConnectedRouter>
+  </Provider>
+);
 
 export default App;
